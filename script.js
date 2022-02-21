b1b736ca7eb651e18260227b96a724b9eecdd1c6
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2022-02-21 19:01:19 +0100

    another commit8

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const apiRoot = 'https://kodilla-task.herokuapp.com/v1/tasks';
+  const trelloApiRoot = 'https://trello.com/grzegorzwaszczynski';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
